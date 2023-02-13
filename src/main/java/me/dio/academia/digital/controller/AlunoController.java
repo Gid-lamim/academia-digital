f27dbe604b7528d22f68a8d7634925bc5831bfa9
Gid.Lamim <gid.lamim@gmail.com>
2023-02-13 11:59:03 -0300

    add AlunoServiceImpl

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,18 @@
 package me.dio.academia.digital.controller;
 
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+
+
 }
