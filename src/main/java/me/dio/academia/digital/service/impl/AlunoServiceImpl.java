83340fd63b409fce5bbbda0a911c997211060734
Gid.Lamim <gid.lamim@gmail.com>
2023-02-13 12:07:06 -0300

    add repository

--- src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -3,13 +3,18 @@ package me.dio.academia.digital.service.impl;
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AlunoServiceImpl implements IAlunoService {
+
+    @Autowired
+    private AlunoRepository repository;
 
     @Override
     public Aluno create(AlunoForm form) {
@@ -23,6 +28,7 @@ public class AlunoServiceImpl implements IAlunoService {
 
     @Override
     public List<Aluno> getAll() {
+
         return null;
     }
 
