41860e4f2b4424113565ade3ba01c1e718e3d3e7
Gid.Lamim <gid.lamim@gmail.com>
2023-02-13 12:25:44 -0300

    fix annotation on AlunoForm

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,7 +1,22 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+//A anotação @Data nos dá os getters and setters
+/*@Data is a convenient shortcut annotation
+that bundles the features of @ToString,
+@EqualsAndHashCode, @Getter / @Setter*/
+
+//@NoArgsConstructor  gera um construtor sem parâmetros
+//@AllArgsConstructor gera um construtor com todos os parâmetros
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunoForm {
 
   private String nome;
@@ -11,4 +26,5 @@ public class AlunoForm {
   private String bairro;
 
   private LocalDate dataDeNascimento;
+
 }
